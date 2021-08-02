896ce3a8d5d85d3d25e1d9d8cc2ada2a8ded95b8
Jenny Woorim Lee <jennylee@jennylees-MacBook-Pro.local>
2021-08-02 13:51:59 +0900

    first commit: project creation

---
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatApp.swift
+//  SwiftUIChat
+//
+//  Created by Jenny Woorim Lee on 2021/08/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ SwiftUIChat/View/Authentication/LoginView.swift
@@ -0,0 +1,23 @@
+//
+//  LoginView.swift
+//  SwiftUIChat
+//
+//  Created by Jenny Woorim Lee on 2021/08/02.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
